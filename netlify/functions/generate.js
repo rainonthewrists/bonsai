06dc1ca8430bfae4b904d4a6362126738515c7f8
rainonthewrists <137094527+rainonthewrists@users.noreply.github.com>
2025-05-25 00:35:11 +0300

    Update generate.js

--- netlify/functions/generate.js
+++ netlify/functions/generate.js
@@ -1,8 +1,11 @@
+const { RiTa } = require('rita');
+
 exports.handler = async (event, context) => {
     try {
+        const word = RiTa.randomWord({ pos: "nn" }); // Генерируем случайное существительное
         return {
             statusCode: 200,
-            body: "Hello from Netlify Function!"
+            body: `Generated word: ${word}`
         };
     } catch (error) {
         return {
